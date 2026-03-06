3e80af2cd2f6f0c4c312eb1a9d5f0cc40c3a8cdf
Aadi Poddar <aadipoddarmail@gmail.com>
2026-03-06 17:40:34 +0530

    Location Filter in Dining Dashboard

--- PrimeBakes/PrimeBakes.Shared/Pages/Restaurant/Bill/DiningDashbaord.razor.cs
+++ PrimeBakes/PrimeBakes.Shared/Pages/Restaurant/Bill/DiningDashbaord.razor.cs
@@ -1,9 +1,13 @@
+using Microsoft.JSInterop;
+using PrimeBakes.Shared.Components.Dialog;
 using PrimeBakesLibrary.Data.Common;
 using PrimeBakesLibrary.Data.Restaurant.Bill;
 using PrimeBakesLibrary.DataAccess;
 using PrimeBakesLibrary.Models.Operations;
 using PrimeBakesLibrary.Models.Restuarant.Bill;
 using PrimeBakesLibrary.Models.Restuarant.Dining;
+using Syncfusion.Blazor.DropDowns;
+using Syncfusion.XlsIO;
 
 namespace PrimeBakes.Shared.Pages.Restaurant.Bill;
 
@@ -13,6 +17,9 @@ public partial class DiningDashbaord : IAsyncDisposable
 	private UserModel _user;
 	private bool _isLoading = true;
 
+	private LocationModel _selectedLocation = new();
+
+	private List<LocationModel> _locations = [];
 	private List<DiningAreaModel> _diningAreas = [];
 	private List<DiningTableModel> _diningTables = [];
 	private List<BillModel> _runningBills = [];
@@ -37,13 +44,38 @@ public partial class DiningDashbaord : IAsyncDisposable
 
 	private async Task LoadData()
 	{
+		_locations = await CommonData.LoadTableDataByStatus<LocationModel>(TableNames.Location);
+		_locations = [.. _locations.OrderBy(s => s.Name)];
+		_selectedLocation = _locations.FirstOrDefault(_ => _.Id == _user.LocationId);
+		
+		await LoadDining();
+	}
+
+	private async Task LoadDining()
+	{
 		_diningAreas = await CommonData.LoadTableDataByStatus<DiningAreaModel>(TableNames.DiningArea);
-		_diningAreas = [.. _diningAreas.Where(area => area.LocationId == _user.LocationId).OrderBy(area => area.Name)];
+		_diningAreas = [.. _diningAreas.Where(area => area.LocationId == _selectedLocation.Id).OrderBy(area => area.Name)];
 
 		_diningTables = await CommonData.LoadTableDataByStatus<DiningTableModel>(TableNames.DiningTable);
 		_diningTables = [.. _diningTables.Where(dt => _diningAreas.Any(area => area.Id == dt.DiningAreaId)).OrderBy(dt => dt.Name)];
 
-		_runningBills = await BillData.LoadRunningBillByLocationId(_user.LocationId);
+		_runningBills = await BillData.LoadRunningBillByLocationId(_selectedLocation.Id);
+	}
+
+	private async Task OnLocationChanged(ChangeEventArgs<LocationModel, LocationModel> args)
+	{
+		if (_user.LocationId > 1)
+		{
+			_selectedLocation = _locations.FirstOrDefault(s => s.Id == _user.LocationId);
+			return;
+		}
+
+		if (args.Value is null)
+			return;
+
+		_selectedLocation = args.Value;
+
+		await LoadDining();
 	}
 
 	private void NavigateToDashboard() =>
